85e047b2d2f55f143f07b687dadbe635af8a1201
zhai <masterzxg@gmail.com>
2016-03-07 19:20:35 +0800

    更新版本

---
+++ app/src/main/java/com/zhaisoft/lib/updater/example/BaseApplication.java
@@ -0,0 +1,19 @@
+package com.zhaisoft.lib.updater.example;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+    String TAG = "BaseApplication";
+    private static BaseApplication instance;
+
+
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        //打开日志开关
+
+
+    }
+
+
+}
